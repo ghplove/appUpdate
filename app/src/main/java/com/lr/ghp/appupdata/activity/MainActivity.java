abb30a1b5a90eb89c928a59f1233f7c2d831126d
huiping.guo <huiping.guo@jimubox.com>
2015-09-21 17:11:12 +0800

    add nomal

---
+++ app/src/main/java/com/lr/ghp/appupdata/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.lr.ghp.appupdata.activity;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.lr.ghp.appupdata.R;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ app/src/main/java/com/lr/ghp/appupdata/constants/Constants.java
@@ -0,0 +1,22 @@
+package com.lr.ghp.appupdata.constants;
+
+import android.os.Environment;
+
+import java.io.File;
+
+/**
+ * Created by ghp on 15/9/14.
+ * 类说明：  常量类
+ */
+public class Constants {
+    //用于测试的packageName
+    public static final String TEST_PACKAGENAME = "com.sina.appupdata";
+
+    public static final String PATH = Environment.getExternalStorageDirectory() + File.separator;
+
+    //合成得到的新版微博
+    public static final String NEW_APK_PATH = PATH + "appOldtoNew.apk";
+
+    //从服务器下载来的查分包
+    public static final String PATCH_PATH = PATH + "app.patch";
+}
